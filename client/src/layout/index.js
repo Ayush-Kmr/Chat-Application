92add91216854eddaefdd1c4ca654d241b5332e5
Ayush Kumar <krayushkumar01@gmail.com>
2024-05-12 01:34:51 +0530

    Initial Commit

---
+++ client/src/layout/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+const AuthLayouts = ({ children }) => {
+  return (
+    <>
+      <header className="flex justify-center items-center py-3 h-20 shadow-md bg-gradient-to-r from-customLightBlue to-customBlue">
+        {" "}
+        <img src={logo} alt="logo" width={180} height={60} />
+      </header>
+
+      {children}
+    </>
+  );
+};
+
+export default AuthLayouts;
---
+++ client/src/pages/CheckEmailPage.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { IoClose } from "react-icons/io5";
+import { Link, useNavigate } from "react-router-dom";
+import uploadFile from "../helpers/uploadFile";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { PiUserCircle } from "react-icons/pi";
+
+const CheckEmailPage = () => {
+  const [data, setData] = useState({
+    email: "",
+  });
+  const navigate = useNavigate();
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const URL = `${process.env.REACT_APP_BACKEND_URL}/api/email`;
+
+    try {
+      const response = await axios.post(URL, data);
+
+      toast.success(response.data.message);
+
+      if (response.data.success) {
+        setData({
+          email: "",
+        });
+        navigate("/password", {
+          state: response?.data?.data,
+        });
+      }
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
+
+  return (
+    <div className="mt-10 ">
+      <div className="bg-gradient-to-b from-customBlue via-customHover to-customDarkBlue w-full max-w-md rounded overflow-hidden p-10 mx-auto">
+        <div className="w-fit mx-auto mb-2">
+          <PiUserCircle size={80} />
+        </div>
+
+        <h3 className="text-center text-xl font-semibold mb-4">
+          Welcome to Chat app!
+        </h3>
+
+        <form className="grid gap-4 mt-3" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email" className="text-sm font-semibold">
+              Email :
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              className="bg-slate-100 px-2 py-1 focus:outline-gray border-0.5 rounded"
+              value={data.email}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-customPrimary to-customSecondary text-lg px-4 py-1 rounded mt-2 font-bold text-white leading-relaxed tracking-wide hover:from-customSecondary hover:to-customPrimary"
+          >
+            Let's Go
+          </button>
+        </form>
+
+        <p className="mt-3 text-center text-sm">
+          New User?{" "}
+          <Link
+            to={"/register"}
+            className="text-gradient-from-customPrimary text-gradient-to-customSecondary font-semibold hover:underline"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CheckEmailPage;
